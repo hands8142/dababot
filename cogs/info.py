43c8cfc73f3f24a56da89bad66ed9917a5cc2708
hands8142 <46277532+hands8142@users.noreply.github.com>
2020-09-12 16:12:59 +0900

    1.0.1 업데이트

--- cogs/info.py
+++ cogs/info.py
@@ -1,5 +1,6 @@
 import discord
 import datetime
+import sys
 from discord.utils import get
 from discord.ext import commands
 
@@ -39,6 +40,23 @@ class info(commands.Cog):
     embed.add_field(name="서버 지역", value=str(ctx.guild.region).title(), inline=True)
     embed.add_field(name="서버 주인", value=ctx.guild.owner.display_name, inline=True)
     embed.add_field(name="서버 만들어진 날짜", value=ctx.guild.created_at.strftime("%y/%m/%d %H:%M:%S"), inline=True)
+    embed.timestamp = datetime.datetime.utcnow()
+    await ctx.send(embed=embed)
+
+  @commands.command(name="봇정보", pass_context=True)
+  async def botinformation(self, ctx):
+    guilds_count = len(self.client.guilds)
+    members_count = 0
+    for guild in self.client.guilds:
+      members_count += len(guild.members)
+    embed = discord.Embed(colour=colour)
+    embed.set_image(url=self.client.user.avatar_url)
+    embed.add_field(name="discord.py", value=discord.__version__, inline=True)
+    embed.add_field(name="python", value=sys.version[:5], inline=True)
+    embed.add_field(name="bot", value="1.0.1", inline=True)
+    embed.add_field(name="user", value=members_count, inline=True)
+    embed.add_field(name="server", value=guilds_count, inline=True)
+    embed.add_field(name="creator", value="한동준#7865")
     embed.timestamp = datetime.datetime.utcnow()
     await ctx.send(embed=embed)
 
